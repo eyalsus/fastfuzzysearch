8b8f201b1497ad043f457fd11883c1ba42894cda
Eyal Paz <eyalp@ad.checkpoint.com>
2021-06-02 13:40:20 +0300

    reduce model size

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if python_version.major < 3:
 setup(
   name='fastfuzzysearch',  
   packages=['fastfuzzysearch'],  
-  version='0.0.3', 
+  version='0.0.4', 
   license='BSD-3-Clause and Public-Domain',
   description='a Python package for fast fuzzy search entirely in-memory based',
   author='Eyal Paz',
